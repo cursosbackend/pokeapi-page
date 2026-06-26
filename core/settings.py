7ed4c6b38784f45c65d0220ce9f96cea831d753c
cursosbackend <cursosbackends@gmail.com>
2026-06-26 01:25:49 +0000

    fix: reemplazar STATICFILES_STORAGE deprecated por STORAGES, agregar runtime.txt

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "pokemon_list"
