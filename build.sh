905cf8ec54df82fdb306f9dea35ea66f4c1df72f
cursosbackend <cursosbackends@gmail.com>
2026-06-26 02:28:51 +0000

    feat: agregar createsuperuser al build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py import_pokemon
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
